bc7180cb1e280ef7174829cfb0f86241175e6848
Evan Lucas <evanlucas@me.com>
2016-01-06 16:41:38 -0600

    utils: add flatten functionality

--- test/utils.js
+++ test/utils.js
@@ -20,3 +20,24 @@ test('encode', (t) => {
   t.equal(utils.encode(input), exp, exp)
   t.end()
 })
+
+test('flatten', (t) => {
+  const input = [
+    { type: 'notice', message: 'A' }
+  , { type: 'notice', message: 'B' }
+  , { type: 'topic', message: 'C', channel: 'D' }
+  , { type: 'topic', message: 'E', channel: 'F' }
+  , { type: 'notice', message: 'G' }
+  , { type: 'topic', message: 'H', channel: 'I' }
+  ]
+  const out = utils.flatten(input)
+  const exp = [
+    { type: 'notice', messages: ['A', 'B'], channel: undefined }
+  , { type: 'topic', messages: ['C'], channel: 'D' }
+  , { type: 'topic', messages: ['E'], channel: 'F' }
+  , { type: 'notice', messages: ['G'], channel: undefined }
+  , { type: 'topic', messages: ['H'], channel: 'I' }
+  ]
+  t.deepEqual(out, exp)
+  t.end()
+})
