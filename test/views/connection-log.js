e601d5cd273a94c8cf4b9985d5f0c235188d441d
Evan Lucas <evanlucas@me.com>
2016-01-09 01:39:31 -0600

    test: add tests for connection log view

---
+++ test/views/connection-log.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const test = require('tap').test
+const ConnectionLog = require('../../lib/views/connection-log')
+const utils = require('../../lib/utils')
+
+test('ConnectionLogView', (t) => {
+  const app = {
+    nav: {}
+  }
+
+  const conn = new ConnectionLog(app)
+  const d = new Date()
+  const log = {
+    ts: d
+  , type: 'notice'
+  , message: 'This is a test. https://github.com'
+  }
+
+  const v = conn.render(log)
+
+  t.equal(v.tagName, 'LI')
+  t.deepEqual(v.properties, { className: 'notice' })
+  const kids = v.children
+  t.equal(kids.length, 2)
+  t.equal(kids[0].tagName, 'SPAN')
+  t.equal(kids[0].properties.className, 'ts')
+  t.equal(kids[0].children[0].text, `[${utils.date(d)}]`)
+
+  const con = kids[1]
+  t.equal(con.tagName, 'SPAN')
+  t.equal(con.properties.className, 'content')
+  t.equal(con.properties.innerHTML,
+    'This is a test. <a href="https://github.com" class="external-url ' +
+    'external-url-url" target="_blank">github.com</a>'
+  )
+
+  t.end()
+})
