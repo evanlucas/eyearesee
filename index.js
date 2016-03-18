6747a0f9b0953938a56fcc44a50e6f8f00376394
Evan Lucas <evanlucas@me.com>
2016-03-18 07:14:17 -0500

    move some logic into lib/app.js
    It will help keep the code cleaner hopefully

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = require('app')
 const path = require('path')
 const electron = require('electron')
 const shortcuts = electron.globalShortcut
+const fs = require('fs')
+const v8 = require('v8')
+v8.setFlagsFromString('--harmony')
 
 const name = 'EyeAreSee'
 const index = `file://${path.join(__dirname, 'views', 'index.html')}`
@@ -20,6 +23,7 @@ app.on('window-all-closed', () => {
 var mainWindow
 
 function setup() {
+  setupHome()
   shortcuts.unregister('ctrl+r')
 
   mainWindow = new BrowserWindow({
@@ -32,9 +36,32 @@ function setup() {
   , title: name
   })
 
+  const App = require('./lib/app')
+  App.create({
+    resourcePath: process.env.EYEARESEE_RESOURCE_PATH
+  , version: require('./package').version
+  , window: mainWindow
+  })
+
   mainWindow.loadURL(index)
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+}
+
+function setupHome() {
+  const e = process.env.EYEARESEE_HOME
+  if (e) return
+
+  let home = path.join(app.getPath('home'), '.eyearesee')
+
+  try {
+    home = fs.realPathSync(home)
+  } catch (_) {
+    // ignore :/
+  }
+
+  process.env.EYEARESEE_HOME = home
+  process.env.EYEARESEE_RESOURCE_PATH = __dirname
 }
